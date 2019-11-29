57f595a8bdecaf94396bffc2347851d773a4d1f7
setaek <43506022+setaek@users.noreply.github.com>
2019-11-29 13:39:02 +0100

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -5,25 +5,22 @@ $(
     //empty.
     describe("RSS Feeds", function() {
       it("AllFeeds Variable are defined and not empty", function() {
-        expect(allFeeds).toBeDefined();
-        expect(allFeeds.length).not.toBe(0);
+        expect(allFeeds).toBeTruthy();
       });
       // test that loops through each feed
       //  in the allFeeds object and ensures it has a URL defined
       //  and that the URL is not empty.
       it("All urls are defined and not empty", function() {
-        allFeeds.forEach(x => {
-          expect(x.url).toBeDefined();
-          expect(x.url.length).not.toBe(0);
+        allFeeds.forEach(feed => {
+          expect(feed.url).toBeTruthy();
         });
       });
       //   test that loops through each feed
       //  in the allFeeds object and ensures it has a name defined
       //  and that the name is not empty.
       it("All Names are Defined and not empty", function() {
-        allFeeds.forEach(x => {
-          expect(x.name).toBeDefined();
-          expect(x.name.length).not.toBe(0);
+        allFeeds.forEach(feed => {
+          expect(feed.name).toBeTruthy();
         });
       });
     });
@@ -60,50 +57,33 @@ $(
       //   a single .entry element within the .feed container.
       let feedContainer;
       beforeEach(function(done) {
-        feedContainer = document.querySelector(".feed");
         loadFeed(0, done);
       });
 
       it("at least a Single element in feed Container", function(done) {
-        let result = feedContainer.children.length > 0;
+        feedContainer = document.querySelectorAll(".feed .entry");
+        let result = feedContainer.length > 0;
         expect(result).toBe(true);
         done();
       });
     });
     // "New Feed Selection" Test
     describe("New Feed Selection", function() {
-      let firstFeed,
-        secondFeed,
-        thirdFeed,
-        FourthFeed,
-        contentArray = [];
-
+      let firstFeed, secondFeed;
       beforeEach(function(done) {
         loadFeed(0, () => {
           firstFeed = document.querySelector(".feed").innerText;
-          contentArray.push(firstFeed);
-        });
-        loadFeed(1, () => {
-          secondFeed = document.querySelector(".feed").innerText;
-          contentArray.push(secondFeed);
-        });
-        loadFeed(2, () => {
-          thirdFeed = document.querySelector(".feed").innerText;
-          contentArray.push(thirdFeed);
-        });
-        loadFeed(3, () => {
-          FourthFeed = document.querySelector(".feed").innerText;
-          contentArray.push(FourthFeed);
-          done();
-        });
 
+          loadFeed(1, () => {
+            secondFeed = document.querySelector(".feed").innerText;
+            done();
+          });
+        });
         // test that ensures when a new feed is loaded
         // the content actually changes.
       });
       it("content changes when new feed loaded", function(done) {
-        let findDuplicatedContent = arr =>
-          arr.filter((item, index) => arr.indexOf(item) != index);
-        expect(findDuplicatedContent(contentArray).length == 0).toBe(true);
+        expect(firstFeed).not.toBe(secondFeed);
         done();
       });
     });
